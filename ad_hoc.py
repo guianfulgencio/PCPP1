5a32aea019c49d3721f435e41bccc41d987f14f8
Vianney Cantos <guiancarlofulgencio@Vianneys-MacBook-Air.local>
2023-03-12 23:00:48 +0800

    updates

--- ad_hoc.py
+++ ad_hoc.py
@@ -1,20 +1,5 @@
-'''class Demo:
-    class_var = 'shared variable'
-    
-    def __init__(self, height, weight, sex):
-        self.height = height
-        self.weight = weight
-        self.sex = sex
-
-print(Demo.class_var)
-print(Demo.__dict__)
-'''
-
-temperature = float(input('Enter current temperature:'))
-
-if temperature > 0:
-    print("Above zero")
-elif temperature < 0:
-    prin("Below zero")
-else:
-    print("Zero")
+lst = [5, 3, 1, 2, 4]
+print(lst)
+ 
+lst.sort(reverse=True)
+print(lst)  # outputs: [1, 2, 3, 4, 5]
